30064816a8225132f9217f63f54a250cbd1588bc
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-25 17:40:46 -0300

    feat: Upload components files with Header component and CSSS module and the styles.css.

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from '../components/Header';
 import { Post } from './Post';
+import './styles.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <h1>Hello World</h1>
       <p>Matheus here!</p>
       <Post author='Matheus Florêncio' content='Esse é o Post do Matheus' />
