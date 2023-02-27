931faeda15a359aa99807ba34f6a612a8979d6cd
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-26 21:14:32 -0300

    feat: App modules css and style of the App component.

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,21 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post />
-      <h1>Hello World</h1>
-      <p>Matheus here!</p>
-      <Post author='Matheus Florêncio' content='Esse é o Post do Matheus' />
-      <Post author='Diego Fernandes' content='Esse é o Post do Diego' />
+
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post author='Matheus Florêncio' content='Esse é o Post do Matheus' />
+          <Post author='Diego Fernandes' content='Esse é o Post do Diego' />
+        </main>
+      </div>
     </>
   );
 }
