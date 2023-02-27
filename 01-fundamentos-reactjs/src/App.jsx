20e4c149d167026390131d230f54ac8659a2a640
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-26 21:30:55 -0300

    feat: Sidebar component and module css and initial style.

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,6 +3,7 @@ import { Post } from './Post';
 
 import './global.css';
 import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
@@ -10,7 +11,7 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-        <aside>sidebar</aside>
+        <Sidebar />
         <main>
           <Post author='Matheus Florêncio' content='Esse é o Post do Matheus' />
           <Post author='Diego Fernandes' content='Esse é o Post do Diego' />
---
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css';
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src='https://images.unsplash.com/photo-1603468620905-8de7d86b781e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'
+      />
+
+      <div className={styles.profile}>
+        <strong>Matheus Florêncio</strong>
+        <span>Software Engineer</span>
+      </div>
+
+      <footer>
+        <a href='#'>Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
