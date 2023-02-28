bafb8e816488333a2fafe2f4c2c998207afca80c
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-02-28 18:30:09 -0300

    feat: Start of Component Comment.

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/florencioMath.png' alt='' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Matheus Florêncio</strong>
+              <time
+                title='27 de Fevereiro às 21:38h '
+                datatime='2023-02-27 21:38:13'
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Matheus, parabéns! 👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
